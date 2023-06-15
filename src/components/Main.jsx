b97b9789a9a7360464cc27dcc4e77894a6b6b664
Donatelo27 <97600974+DonBos27@users.noreply.github.com>
2023-06-15 21:05:18 +0200

    Add title

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,8 +22,12 @@ function Main() {
         <img
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt=""
-          className="w-full h-full object-cover filter brightness-50 "
+          className="w-full h-full object-cover filter brightness-50 opacity-30"
         />
+        <h1 className="absolute top-[160px] left-[70px] text-4xl font-bold">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        
       </div>
     </div>
   );
