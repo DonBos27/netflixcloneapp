3065218fe9ebb0d48e1162da927ee670e3c3efba
Donatelo27 <97600974+DonBos27@users.noreply.github.com>
2023-10-30 12:26:26 +0200

    Makes some pages responsive

--- src/components/Navabar.jsx
+++ src/components/Navabar.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import NetflixLogo from '../images/netflixLogo.png'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import NetflixLogo from "../images/netflixLogo.png";
+import { useNavigate } from "react-router-dom";
 
 function Navabar() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const signIn = () => {
-    navigate('/home') 
-  }
+    navigate("/home");
+  };
   return (
-    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <img src={NetflixLogo} alt="" className='w-[250px] h-full pl-7'/>
-        <div>
-            <button className='bg-red-600 text-white text-xl font-bold px-7 py-2 mr-[65px] rounded cursor-pointer' onClick={signIn}>Sign In</button>
-        </div>
+    <div className="flex items-center justify-between lg:p-4 z-[100] w-full absolute">
+      {/* Adjusting the logo's size based on screen size */}
+      <img
+        src={NetflixLogo}
+        alt="Netflix Logo"
+        className="w-60 md:w-64 lg:w-[250px] lg:pl-7 h-full"
+      />
+
+      <div>
+        {/* Adjusting the button's size based on screen size */}
+        <button
+          className="text-sm md:text-base lg:text-xl bg-red-600 text-white font-bold px-4 md:px-5 lg:px-7 py-1 md:py-2 rounded cursor-pointer mr-4 md:mr-5 lg:mr-[65px]"
+          onClick={signIn}
+        >
+          Sign In
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navabar
+export default Navabar;
